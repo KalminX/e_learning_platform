3d0d39921de0d35b3eda9276c6e66eeb7d20a75a
KalminX <noriabure@gmail.com>
2024-10-11 12:04:11 +0100

    one big commit

---
+++ educa/educa/settings/prod.py
@@ -0,0 +1,33 @@
+from .base import *
+from decouple import config
+
+DEBUG = False
+
+ADMINS = [
+    ('Numerous O', 'noriabure@gmail.com'),
+]
+
+ALLOWED_HOSTS = ['educaproject.com', 'www.educaproject.com']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+REDIS_URL = 'redis://cache:6379'
+
+CACHES['default']['LOCATION'] = REDIS_URL
+
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
+
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+# # SECURE_SSL_REDIRECT = True
+
